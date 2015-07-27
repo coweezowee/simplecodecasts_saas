b05faabe9ef954e1a3e75c13caa2500ec68b2006
Aidan Weeks <aw4391@gmail.com>
2015-07-27 20:59:22 +0000

    Modified devise for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723211548) do
+ActiveRecord::Schema.define(version: 20150727204224) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
